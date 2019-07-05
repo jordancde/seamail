bd2af65fd41456bd03e4acec43abeea41c759c9a
Nikhil Gupta <niks.gupta09@gmail.com>
2019-07-05 14:12:59 -0400

    bringing changes over

--- src/providers/emailProvider.cc
+++ src/providers/emailProvider.cc
@@ -1,5 +1,17 @@
 #include "emailProvider.h"
 
+using namespace std;
+
 EmailProvider::EmailProvider() {}
 
 EmailProvider::~EmailProvider() {}
+
+ostream& EmailProvider::serialize(ostream& sout) const {
+    
+    return sout;
+}
+
+istream& EmailProvider::deserialize(istream& sin) {
+
+    return sin;
+}
