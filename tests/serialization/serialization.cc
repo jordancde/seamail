01505485abd7f3a90785d74060361f42075501d2
Jordan Dearsley <jordandearsley67@gmail.com>
2019-07-09 11:59:57 -0400

    tests WIP

---
+++ tests/serialization/serialization.cc
@@ -0,0 +1,42 @@
+#include <fstream>
+#include <iostream>
+#include "providers/localEmailProvider.h"
+#include "utility/localState.h"
+
+using namespace std;
+
+int main() {
+    LocalState ls = LocalState();
+    ls.localProvider = LocalEmailProvider();
+    shared_ptr<LocalEmailProvider> lmp{&(ls.localProvider)};
+
+    auto myDummyAccount =
+        std::make_shared<Account>(lmp, "mydummyaccount@example.com");
+    ls.localProvider.addAccount("mydummyaccount@example.com", "abc123");
+
+    ls.storeAccount(*myDummyAccount);
+    myDummyAccount->login("mydummyaccount@example.com", "abc123");
+
+    Email e = Email("new", "mydummyaccount@example.com",
+                    vector<string>{"mydummyaccount2@example.com"}, time(NULL),
+                    vector<string>{}, vector<string>{}, "test subject",
+                    "test body", false, vector<string>{});
+
+    myDummyAccount->sendEmail(e);
+
+    ofstream outFile;
+    outFile.open("serialized.txt");
+    outFile << ls;
+    outFile.close();
+
+    ifstream inFile;
+
+    inFile.open("serialized.txt");
+
+    LocalState inputLS = LocalState();
+    inFile >> inputLS;
+    inFile.close();
+
+    
+    return 0;
+}
